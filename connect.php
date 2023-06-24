a6465d32850db444d24cadf4769f734df91aa4da
minhvqgcc210229 <121932119+minhvqgcc210229@users.noreply.github.com>
2023-06-24 09:31:31 +0700

    update
    update

--- connect.php
+++ connect.php
@@ -8,9 +8,9 @@
         public function __construct()
         {
             $this->server ="co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-            $this->user ="	w5tu1m1b35khyzh8";
-            $this->password ="pu651oeqhod84ar1";
-            $this->dbname = "veqadsufindlic4z";
+            $this->user ="kt13jdhiey5ph28t";
+            $this->password ="i4fkblumse39l14w";
+            $this->dbname = "rp32sbaene5i33q7";
         }
         //option 1: use mySQLi
         function connectToMySQL(): mysqli{
@@ -35,5 +35,5 @@
         }
     }
     // //test connect
-    $conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','w5tu1m1b35khyzh8','pu651oeqhod84ar1','veqadsufindlic4z') or die("can not connect database" .mysqli_connect_error());
+    $conn = mysqli_connect('co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','kt13jdhiey5ph28t','i4fkblumse39l14w','rp32sbaene5i33q7') or die("can not connect database" .mysqli_connect_error());
 ?>
